866d03713e832b801eddb2f66219d75085d14a4d
blancojohn <142819007+blancojohn@users.noreply.github.com>
2023-11-29 17:25:09 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
 };
+//write your code here
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".net", ".us", ".io"];
+
+let nombresDominios = [];
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        let dominioGenerado = pronoun[i] + adj[j] + noun[k] + extension[l];
+        nombresDominios.push(dominioGenerado);
+      }
+    }
+  }
+}
+console.log("Mis dominios son:", nombresDominios);
